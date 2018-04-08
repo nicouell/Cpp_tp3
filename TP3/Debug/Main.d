215b653f610a52768e2c540439c9c177c4f99ead
Nicola Ouellet <nicola.ouellet@gmail.com>
2018-04-07 21:00:56 -0400

    Premier commit

---
+++ TP3/Debug/Main.d
@@ -0,0 +1,6 @@
+Main.o: ../Main.cpp /home/etudiant/Bureau/TP3/source/Date.h \
+ /home/etudiant/Bureau/TP3/source/ContratException.h
+
+/home/etudiant/Bureau/TP3/source/Date.h:
+
+/home/etudiant/Bureau/TP3/source/ContratException.h:
---
+++ TP3/Main.cpp
@@ -0,0 +1,19 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: 2018-04-04
+ *      Author: etudiant
+ */
+
+#include <iostream>
+#include "Date.h"
+
+using namespace std;
+using namespace util;
+
+int main(){
+	Date date1();
+	return 0;
+}
+
+
