eb1c169cce5edc6d5850cf1283f3dd2eff28b0a6
Nicola Ouellet <nicola.ouellet@gmail.com>
2018-04-11 17:17:00 -0400

    ClientTesteur fixture

--- TP3/Debug/Main.d
+++ TP3/Debug/Main.d
@@ -1,9 +1,11 @@
 Main.o: ../Main.cpp /home/etudiant/Bureau/TP3/source/Client.h \
  /home/etudiant/Bureau/TP3/source/Date.h \
  /home/etudiant/Bureau/TP3/source/ContratException.h \
+ /home/etudiant/Bureau/TP3/source/Cheque.h \
+ /home/etudiant/Bureau/TP3/source/Compte.h \
+ /home/etudiant/Bureau/TP3/source/Epargne.h \
  /home/etudiant/Bureau/TP3/source/validationFormat.h \
  /home/etudiant/Bureau/TP3/source/Epargne.h \
- /home/etudiant/Bureau/TP3/source/Compte.h \
  /home/etudiant/Bureau/TP3/source/Cheque.h
 
 /home/etudiant/Bureau/TP3/source/Client.h:
@@ -12,10 +14,14 @@ Main.o: ../Main.cpp /home/etudiant/Bureau/TP3/source/Client.h \
 
 /home/etudiant/Bureau/TP3/source/ContratException.h:
 
-/home/etudiant/Bureau/TP3/source/validationFormat.h:
+/home/etudiant/Bureau/TP3/source/Cheque.h:
+
+/home/etudiant/Bureau/TP3/source/Compte.h:
 
 /home/etudiant/Bureau/TP3/source/Epargne.h:
 
-/home/etudiant/Bureau/TP3/source/Compte.h:
+/home/etudiant/Bureau/TP3/source/validationFormat.h:
+
+/home/etudiant/Bureau/TP3/source/Epargne.h:
 
 /home/etudiant/Bureau/TP3/source/Cheque.h:
